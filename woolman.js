45955db63d58498e8999d610398c5bff976c646f
UMRnInside <newpage2@foxmail.com>
2020-09-26 19:49:04 +0800

    Revert "Revert "Anti-anti-bot update?""
    This reverts commit ef83c383eda52092c4298b3341609982d1a145bd.

--- woolman.js
+++ woolman.js
@@ -30,6 +30,13 @@ const sheeputil = require("./lib/sheeputil.js")
 global.bot = bot
 global.ad_active = true
 
+global.last_status = {
+    bot_position: Vec3(0, 0, 0),
+    wools: 0,
+    repeats: 0,
+    failures: 0
+}
+
 const colors = [
     "white", "orange", "magenta", "light blue",
     "yellow", "lime", "pink", "gray",
@@ -156,7 +163,7 @@ function ResumeWork()
     if (global.cmd1)
         setTimeout(() => bot.chat(global.cmd1) , 1000);
     if (global.cmd2)
-        setTimeout(() => bot.chat(global.cmd2) , 4500);
+        setTimeout(() => bot.chat(global.cmd2) , 5000);
     setTimeout(autowork, 9000)
 }
 
@@ -187,6 +194,10 @@ bot.on('login', function()
 bot.on('kicked', (reason) => {
     console.log(`I got kicked for`, reason)
     process.exit(0)
+})
+
+bot.on('spawn', () => {
+    console.log(`Spawned at (floored) ${bot.entity.position.floored()}`)
 })
 
 function goShearSheep(target, callback)
@@ -267,14 +278,57 @@ function autowork()
             Reset()
             return
         }
+        console.log(`I'm at ${bot.entity.position.floored()}`)
 
         var sheep = sheeputil.findAvailableSheep(bot)
         if (sheep)
         {
-            console.log("Found sheep")
+            console.log("Found sheep, updating last status")
             goShearSheep(sheep, function() {
                 const wools_in_inventory = inventory.countItemById(82, 97)
                 console.log("wools", wools_in_inventory)
+                if (last_status.bot_position.equals(bot.entity.position) && 
+                    last_status.wools === wools_in_inventory)
+                {
+                    console.log(`Stats unchanged!`)
+                    last_status.repeats += 1
+                    if (last_status.repeats >= global.StatsUnchangedTolerance)
+                    {
+                        if (last_status.total_failure >= 3)
+                        {
+                            console.log(`CRITICAL ERROR!`)
+                            process.exit(1)
+                        }
+                        console.log(`Stats repeated too much!`)
+                        console.log(`Bot position: ${bot.entity.position}`)
+                        console.log(`Target sheep:`)
+                        console.log(sheep)
+
+                        last_status.total_failure += 1
+
+                        if (global.StatsUnchangedPreferReset)
+                        {
+                            Reset()
+                        }
+                        else
+                        {
+                            setTimeout(() => {
+                                console.log("idle to escape...")
+                                const shear_cb = () => setTimeout(autoshear, 5000)
+                                navigator.goToWork(global.idlePosition, shear_cb, Reset)
+                            }, 1000)
+                        }
+
+                        return
+                    }
+                }
+                else
+                {
+                    last_status.bot_position = bot.entity.position
+                    last_status.wools = wools_in_inventory
+                    last_status.repeats = 0
+                }
+
                 if (wools_in_inventory >= 128)
                 {
                     setTimeout(autostore, 1000)
@@ -300,11 +354,12 @@ function autowork()
         if (global.StorageRequiresTp)
         {
             console.log("StorageRequiresTp")
+            global.working = false
             if (global.storage_cmd1)
                 setTimeout(() => bot.chat(global.storage_cmd1) , 1000);
             if (global.storage_cmd2)
-                setTimeout(() => bot.chat(global.storage_cmd2) , 4500);
-            setTimeout( () => autotoss(ResumeWork), 8000)
+                setTimeout(() => bot.chat(global.storage_cmd2) , 5000);
+            setTimeout( () => autotoss(ResumeWork), 12000)
             return
         }
 
